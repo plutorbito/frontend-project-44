9c4fad3289548354050b7d88195886ec21f47ab9
plutorbito <plutorbito@gmail.com>
2023-06-16 14:35:05 +0200

    brain-gcd game added

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import gcdGame from "../src/games/brain-gcd-logic.js";
+
+gcdGame();
---
+++ src/games/brain-gcd-logic.js
@@ -0,0 +1,21 @@
+import { generateRandomNumber, gameLogic } from '../index.js';
+
+const message = 'Find the greatest common divisor of given numbers.';
+
+const gcdGame = () => {
+   for (let i = 0; i < 3; i += 1) {
+      let randomNumber1 = generateRandomNumber(10);
+      let randomNumber2 = generateRandomNumber(10);
+      const task = `${randomNumber1}  ${randomNumber2}`;
+      let rightAnswer;
+      for (let divisor = 1; divisor <= Math.min(randomNumber1, randomNumber2); divisor += 1) {
+         if (randomNumber1 % divisor === 0 && randomNumber2 % divisor === 0) {
+            rightAnswer = divisor;
+         }
+      }
+      return [task, rightAnswer];
+   }
+}
+
+gameLogic(message, gcdGame);
+export default gcdGame;
