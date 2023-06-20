c5825e9a53876114e39731e015bcaa0a0f0ab9fe
plutorbito <plutorbito@gmail.com>
2023-06-20 15:37:05 +0200

    task is updated

--- src/games/brain-gcd-logic.js
+++ src/games/brain-gcd-logic.js
@@ -5,7 +5,7 @@ const message = 'Find the greatest common divisor of given numbers.';
 const gcdGame = () => {
       let randomNumber1 = generateRandomNumber(10);
       let randomNumber2 = generateRandomNumber(10);
-      const task = `${randomNumber1}  ${randomNumber2}`;
+      const task = `${randomNumber1} ${randomNumber2}`;
       let rightAnswer;
       for (let divisor = 1; divisor <= Math.min(randomNumber1, randomNumber2); divisor += 1) {
          if (randomNumber1 % divisor === 0 && randomNumber2 % divisor === 0) {
